846fab0bf00602e6170017dbf6c31c44755c02be
Cho-El <syzzang21c@gmail.com>
2023-04-29 02:04:16 +0900

    feat: insert update 구현
    JDBC Templates를 이용
    - Insert 구현
    - Update 구현

--- src/main/java/com/example/fastcampusmysql/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/repository/MemberRepository.java
@@ -1,11 +1,46 @@
 package com.example.fastcampusmysql.domain.member.repository;
 
 import com.example.fastcampusmysql.domain.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
+@RequiredArgsConstructor
 @Repository
 public class MemberRepository {
+    static final String TABLE = "Member";
+    final private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     public Member save(Member member) {
+        if (member.getId() == null)
+            return insert(member);
+        return update(member);
+    }
+
+    private Member insert(Member member) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate())
+                .withTableName(TABLE)
+                .usingGeneratedKeyColumns("id");
+
+        SqlParameterSource params = new BeanPropertySqlParameterSource(member);
+        var id = jdbcInsert.executeAndReturnKey(params).longValue();
+
+        return Member.builder()
+                .id(id)
+                .nickname(member.getNickname())
+                .email(member.getEmail())
+                .birthday(member.getBirthday())
+                .build();
+    }
+
+    private Member update(Member member) {
+        var sql = String.format("UPDATE %s set email = :email, nickname = :nickname, birthday = :birthday WHERE id = :id", TABLE);
+        SqlParameterSource params = new BeanPropertySqlParameterSource(member);
+        namedParameterJdbcTemplate.update(sql, params);
         return member;
     }
 }
