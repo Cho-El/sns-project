6ca555ffb8a2f0ccd61f4342a608b5ce51fdc3b7
Cho-El <syzzang21c@gmail.com>
2023-05-02 17:57:16 +0900

    refactor: 파일정리

--- src/main/java/com/example/fastcampusmysql/domain/member/entity/Member.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/entity/Member.java
@@ -32,7 +32,12 @@ public class Member {
         this.createdAt = createdAt == null ? LocalDateTime.now() : createdAt;
     }
 
-    void validateNickname(String nickname) {
+    public void changeNickname(String other) {
+        Objects.requireNonNull(other);
+        validateNickname(other);
+        nickname = other;
+    }
+    public void validateNickname(String nickname) {
         Assert.isTrue(nickname.length() <= NAME_MAX_LENGTH, "최대 길이를 초과했습니다.");
     }
 }
