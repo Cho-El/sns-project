5b19fd43db3aa6f316f7f190477975ddafb1558a
Cho-El <syzzang21c@gmail.com>
2023-06-23 21:41:12 +0900

    feat: pagination
    - PostRepostiory
     - Post Row mapper 생성
     - page에 따라 회원 조회 함수 생성
    - PostReadService : 페이지에 따라 회원 조회 서비스

--- src/main/java/com/example/snsmysql/domain/post/service/PostReadService.java
+++ src/main/java/com/example/snsmysql/domain/post/service/PostReadService.java
@@ -2,10 +2,14 @@ package com.example.snsmysql.domain.post.service;
 
 import com.example.snsmysql.domain.post.dto.DailyPostCount;
 import com.example.snsmysql.domain.post.dto.DailyPostCountRequest;
+import com.example.snsmysql.domain.post.entity.Post;
 import com.example.snsmysql.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -16,5 +20,9 @@ public class PostReadService {
 
     public List<DailyPostCount> getDailyPostCount(DailyPostCountRequest request) {
         return postRepository.groupByCreatedDate(request);
+    }
+
+    public Page<Post> getPosts(Long memberId, PageRequest pageRequest) {
+        return postRepository.findAllByMemberId(memberId, pageRequest);
     }
 }
